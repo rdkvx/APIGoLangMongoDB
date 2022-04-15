811c1675056108c41228275adf5e2ac5c2f3750b
Rodrigo Soares <digoftw@hotmail.com>
2022-04-14 23:04:49 -0300

    Persistencia em slice concluida

--- repositorio/conexaoMongo.go
+++ repositorio/conexaoMongo.go
@@ -1,1 +1,20 @@
 package repositorio
+
+import (
+	
+	"fmt"
+	
+
+	
+	"gopkg.in/mgo.v2"
+)
+
+var SessaoMongo *mgo.Session
+
+func AbreSessao() (err error){
+	SessaoMongo, err = mgo.Dial("mongodb+srv://rdkvx:Dragonforce123@cluster0.ntd1x.mongodb.net/test")
+	if err != nil{
+		fmt.Print("Erro ao abrir sessao")
+	}
+	return
+}
